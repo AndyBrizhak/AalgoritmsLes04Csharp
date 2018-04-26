27fa15a4cb6bd836d8f34f0dd562cc1178f2fcc4
AndyBrizhak <andrewbrizhak@gmail.com>
2018-04-26 13:51:51 +0300

    начало ввода рекурсивной процедуры волны ли

--- AlgoritmLee/Program.cs
+++ AlgoritmLee/Program.cs
@@ -16,6 +16,47 @@ namespace AlgoritmLee
 {
     class Program
     {
+
+        public const int X = 3;
+        public const int Y = 3;
+
+        public static void WaweLee(int x, int y, int len, int[,] myArr)
+        {
+            LeftWawe(x, y, len, myArr);
+            UpWawe(x, y, len, myArr);
+            RightWawe(x, y, len, myArr);
+        }
+
+        public static void UpWawe(int x, int y, int len, int[,] myArr)
+        {
+            int upy = y + 1;
+            if (upy >= 0 && upy < Y && myArr[x, upy] == 0)
+            {
+                myArr[x, upy] = len + 1;
+                WaweLee(x, upy, len + 1, myArr);
+            }
+        }
+
+        public static void RightWawe(int x, int y, int len, int[,] myArr)
+        {
+            int rightx = x + 1;
+            if (rightx >= 0 && rightx < X && myArr[rightx, y] == 0)
+            {
+                myArr[rightx, y] = len + 1;
+                WaweLee(rightx, y, len + 1, myArr);
+            }
+        }
+
+        public static void LeftWawe(int x, int y, int len, int[,] myArr)
+        {
+            int leftx = x - 1;
+            if (leftx >=0 && leftx < X && myArr[leftx, y] == 0 )
+	        {
+                myArr[leftx, y] = len + 1;
+                WaweLee(leftx, y, len + 1, myArr);
+            }
+        }
+
         static void Main(string[] args)
         {
             int[,] myArr = new int[3, 3];
@@ -33,6 +74,13 @@ namespace AlgoritmLee
             myArr[0, 1] = -1;
             myArr[0, 2] = -1;
             myArr[2, 1] = -1;
+
+            int x = 0;
+            int y = 0;
+            int len = 0;
+
+            WaweLee(x, y, len, myArr);
+
 
         }
     }
